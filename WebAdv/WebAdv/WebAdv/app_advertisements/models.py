789a6781ca7d84ab8646ff1d77bb921880e6e177
Carman <carman084@mail.ru>
2023-08-22 18:18:00 +0300

    ДЗ № 10

--- WebAdv/WebAdv/WebAdv/app_advertisements/models.py
+++ WebAdv/WebAdv/WebAdv/app_advertisements/models.py
@@ -16,6 +16,11 @@ class Advertisement(models.Model):
     user = models.ForeignKey(User, verbose_name='пользователь', on_delete=models.CASCADE)
     image = models.ImageField('изображение', upload_to='advertisements')
 
+    @admin.display(description='фото')
+    def get_html_image(self):
+        if self.image:
+            return format_html('<img src="{url}" style="max-width: 80px; max-height: 80px;">', url=self.image.url)
+    
     @admin.display(description='дата создания')
     def created_date(self):
         if self.created_at.date() == timezone.now().date():
@@ -29,10 +34,6 @@ class Advertisement(models.Model):
             updated_time = self.updated_at.time().strftime("%H:%M:%S")
             return format_html('<span style="color: green; font-weight: bold;">Сегодня в {}</span>', updated_time)
         return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
-    
-    @admin.display(description='изображение')
-    def img(self):
-        return format_html(f'<img src={self.image}>')
 
     def __str__(self) -> str:
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
